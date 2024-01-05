a368f4a71524810d37bfb1ea5691c53ec62dc735
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-01-05 10:41:19 -0500

    Se añadieron las funcionalidades de cerrar y abrir la ventana del product-detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,14 +4,29 @@ export const ShoppingCartContext = createContext();
 
 
 export const ShoppingCartProvider = ({children}) => {
+    //shopping cart -increment quantity
+    
     const [count, setCount] = useState(0);
     
+    //Product detail - open/close
+    const [isProductDetailOpen, setProductDetailOpen] = useState(false);    
+    const openProductDetail = () => setProductDetailOpen(true);
+    const closeProductDetail = () => setProductDetailOpen(false);
+
+    //Product detail- show product
+    const [productToShow, setProductToShow] = useState({});
+
 
     return (
 
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
         }} >
             {children}
         </ShoppingCartContext.Provider>
