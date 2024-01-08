63e10c28dfa90c2baa7f77212396a9afb783f847
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-01-07 21:39:05 -0500

    Se añadieron las funcionalidades para sumar los productos

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,11 +2,16 @@ import { useContext } from 'react';
 import { XMarkIcon} from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../OrderCard';
+import { totalPrice } from '../../Utils';
 import './style.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext);
     
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id);
+        context.setCartProducts(filteredProducts)
+    }
 
     return (
         <aside 
@@ -25,12 +30,20 @@ const CheckoutSideMenu = () => {
                     context.cartProducts.map(product => (
                         <OrderCard 
                             key={product.id}
+                            id={product.id}
                             title={product.title} 
                             imageUrl={product.image}
                             price={product.price}
+                            handleDelete={handleDelete}
                         />
                     ))
                 }
+            </div>
+            <div className='px-6'>
+                <p className='flex justify-between items-center'>
+                    <span className='font-light'>Total:</span>
+                    <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+                </p>
             </div>
         
             
