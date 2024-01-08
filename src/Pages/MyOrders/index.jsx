65a98d338145d2a8cdcf7d689f6c7ecb90bff3bb
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-01-08 11:07:39 -0500

    Se realizó la maquetación de la lista de ordenes

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,10 +1,31 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../../Components/Layout"
+import OrdersCard from "../../Components/OrdersCard"
+import { ShoppingCartContext } from "../../Context";
 
 function MyOrders() {
+    
+    const context = useContext(ShoppingCartContext)
+
     return (
       
         <Layout>
-            MyOrders
+            <div className="flex items-center justify-center relative w-80">
+                <h1> My Orders</h1>
+            </div>
+           
+            {
+                context.order.map((order, index) => {
+                    <Link key={index} to={`/my-orders/${order.id}`}>
+                        <OrdersCard 
+                            totalPrice={order.totalPrice} 
+                            totalProducts={order.totalProducts} 
+                        />
+                    </Link>
+                    
+                })
+            }
         </Layout>
           
         
