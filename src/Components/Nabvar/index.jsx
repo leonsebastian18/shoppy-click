cac43b34b011436f74dd84d3930a2cc63d3c119d
Sebastian Leon <sebastianleonvillarreal@gmail.com>
2024-01-10 22:59:50 -0500

    Se realizo unos ajustes de estilo de las cards

--- src/Components/Nabvar/index.jsx
+++ src/Components/Nabvar/index.jsx
@@ -1,37 +1,36 @@
-import { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import { ShoppingCartContext } from "../../Context";
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
 import ShoppingCart from '../ShoppingCart'
 
-const Nabvar = () => {
-    const context = useContext(ShoppingCartContext)
-    const activeStyle = 'underline underline-offset-4'
+const Navbar = () => {
+  const context = useContext(ShoppingCartContext)
+  const activeStyle = 'underline underline-offset-4'
 
-//sign-out
-const signOut = localStorage.getItem('sign-out')
-const parsedSignOut = JSON.parse(signOut)
-const isUserSignOut = context.signOut || parsedSignOut
+  // Sign Out
+  const signOut = localStorage.getItem('sign-out')
+  const parsedSignOut = JSON.parse(signOut)
+  const isUserSignOut = context.signOut || parsedSignOut
+  // Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
-//Account 
-const account = localStorage.getItem('account')
-const parsedAccount = JSON.parse(account)
-//Has an account
-const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
-const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
-const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
-
-const handleSignOut = () => {
+  const handleSignOut = () => {
     const stringifiedSignOut = JSON.stringify(true)
     localStorage.setItem('sign-out', stringifiedSignOut)
     context.setSignOut(true)
-}
+  }
 
-const renderView = () => {
-    if (hasUserAnAccount && isUserSignOut) {
-        return (
-            <>
+  const renderView = () => {
+    if (hasUserAnAccount && !isUserSignOut) {
+      return (
+        <>
           <li className='text-black/60'>
-          shoppyclick@gmail.com
+            {parsedAccount?.email}
           </li>
           <li>
             <NavLink
@@ -56,99 +55,98 @@ const renderView = () => {
             </NavLink>
           </li>
         </>
-        )
+      )
     } else {
-        return (
-            <li>
-            <NavLink
-              to="/sign-in"
-              className={({ isActive }) => isActive ? activeStyle : undefined }
-              onClick={() => handleSignOut()}>
-              Sign in
-            </NavLink>
-          </li>
-        )
+      return (
+        <li>
+          <NavLink
+            to="/sign-in"
+            className={({ isActive }) => isActive ? activeStyle : undefined }
+            onClick={() => handleSignOut()}>
+            Sign in
+          </NavLink>
+        </li>
+      )
     }
+  }
+
+  return (
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-white'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to={`${isUserSignOut ? '/sign-in' : '/'}`}>
+          Shoppy-click
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/'
+            onClick={() => context.setSearchByCategory()}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/jewelery'
+            onClick={() => context.setSearchByCategory('jewelery')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Jewelery
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Electronics
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/men's clothing"
+            onClick={() => context.setSearchByCategory("men's clothing")}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Men's clothing
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/women's clothing"
+            onClick={() => context.setSearchByCategory("women's clothing")}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Women's clothing
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/others'
+            onClick={() => context.setSearchByCategory('others')}
+            className={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>
+            Others
+          </NavLink>
+        </li>
+      </ul>
+      <ul className='flex items-center gap-3'>
+        {renderView()}
+        <li className='flex items-center'>
+          <ShoppingCart />
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
-    return (
-        <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
-            <ul className="flex items-center gap-3">
-                <li className="font-semibold text-lg">
-                    <NavLink to={`${isUserSignOut ? '/sign-in' : '/'}`}>
-                        Shoppy-click
-                    </NavLink>
-                </li>
-
-                <li>
-                    <NavLink 
-                        to='/'
-                        onClick={() => context.setSearchByCategory()}
-                        className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
-                        All
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/jewelery'
-                        onClick={() => context.setSearchByCategory('jewelery')}
-                        className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
-                        Jewelery
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/electronics'
-                        onClick={() => context.setSearchByCategory('electronics')}
-                        className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
-                        Electronics
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to="/men's clothing"
-                        onClick={() => context.setSearchByCategory("men's clothing")}
-                        className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
-                        Men's Clothing
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to="/women's clothing"
-                        onClick={() => context.setSearchByCategory("women's clothing")}
-                        className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
-                        Women's Clothing
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink 
-                        to='/others'
-                        onClick={() => context.setSearchByCategory('others')}
-                        className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                    }>
-                        Others
-                    </NavLink>
-                </li>
-            </ul>
-            <ul className="flex items-center gap-3">
-                {renderView()}
-                <li className="flex items-center">
-                    <ShoppingCart/>
-                </li>
-            </ul>
-        </nav>
-    );
-}
-
-export default Nabvar
+export default Navbar
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,8 +1,11 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react'
+
 
 import { apiUrl } from "../Api";
 
-export const ShoppingCartContext = createContext();
+
+
+export const ShoppingCartContext = createContext()
 
 export const initializeLocalStorage = () => {
   const accountInLocalStorage = localStorage.getItem('account')
@@ -25,132 +28,123 @@ export const initializeLocalStorage = () => {
   }
 }
 
-
 export const ShoppingCartProvider = ({children}) => {
-  //my account
-  const [account, setAccount] = useState({});
-  
-  //sign out
-  const [signOut, setSignOut] = useState(false);
-  
-  
-  //shopping cart -increment quantity
-    
-    const [count, setCount] = useState(0);
-    
-    //Product detail - open/close
-    const [isProductDetailOpen, setProductDetailOpen] = useState(false);    
-    const openProductDetail = () => setProductDetailOpen(true);
-    const closeProductDetail = () => setProductDetailOpen(false);
+  // My account
+  const [account, setAccount] = useState({})
 
-        //Checkout side Menu - open/close
-        const [isCheckoutSideMenuOpen, setCheckoutSideMenuOpen] = useState(false);    
-        const openCheckoutSideMenu = () => setCheckoutSideMenuOpen(true);
-        const closeCheckoutSideMenu = () => setCheckoutSideMenuOpen(false);
-    
-    //Product detail- show product
-    const [productToShow, setProductToShow] = useState({});
+  // Sign out
+  const [signOut, setSignOut] = useState(false)
 
-    //Shopping cart-add products to cart
-    const [cartProducts, setCartProducts] = useState([]);
+  // Shopping Cart · Increment quantity
+  const [count, setCount] = useState(0)
 
+  // Product Detail · Open/Close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
 
-    //shopping cart - order
-    const [order, setOrder] = useState([]);
+  // Checkout Side Menu · Open/Close
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
 
-    //Get products
-    const [items, setItems] = useState(null);
-    const [filteredItems, setFilteredItems] = useState(null);
+  // Product Detail · Show product
+  const [productToShow, setProductToShow] = useState({})
 
-    //Get products by title
-    const [searchByTitle, setSearchByTitle] = useState(null)
+  // Shopping Cart · Add products to cart
+  const [cartProducts, setCartProducts] = useState([])
 
-    //Get products by category
-    const [searchByCategory, setSearchByCategory] = useState(null)
-    
-    useEffect(() => {
+  // Shopping Cart · Order
+  const [order, setOrder] = useState([])
 
-        const fetchData = async () => {
-          try {
-            const response = await 
-            fetch (`${apiUrl}`) 
-              const data = await 
-              response.json()
-              setItems(data)
-          } catch (error) {
-            console.error(`oh no, ocurrió un error: ${error}`);
-          }
-        }
-       fetchData()
-    }, []);
-  
+  // Get products
+  const [items, setItems] = useState(null)
+  const [filteredItems, setFilteredItems] = useState(null)
 
-    const filteredItemsByTitle = (items, searchByTitle) => {
-        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  // Get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  // Get products by category
+  const [searchByCategory, setSearchByCategory] = useState(null)
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await 
+        fetch (`${apiUrl}`) 
+          const data = await 
+          response.json()
+          setItems(data)
+      } catch (error) {
+        console.error(`oh no, ocurrió un error: ${error}`);
       }
-    
-      const filteredItemsByCategory = (items, searchByCategory) => {
-        return items?.filter(item => item.category.toLowerCase().includes(searchByCategory.toLowerCase()))
-      }
-    
-      const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
-        if (searchType === 'BY_TITLE') {
-          return filteredItemsByTitle(items, searchByTitle)
-        }
-    
-        if (searchType === 'BY_CATEGORY') {
-          return filteredItemsByCategory(items, searchByCategory)
-        }
-    
-        if (searchType === 'BY_TITLE_AND_CATEGORY') {
-          return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
-        }
-    
-        if (!searchType) {
-          return items
-        }
-      }
+    }
+   fetchData()
+}, []);
 
-      useEffect(() => {
-        if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
-        if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
-        if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
-        if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
-      }, [items, searchByTitle, searchByCategory])
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
 
+  const filteredItemsByCategory = (items, searchByCategory) => {
+    return items?.filter(item => item.category.name.toLowerCase().includes(searchByCategory.toLowerCase()))
+  }
 
-    return (
+  const filterBy = (searchType, items, searchByTitle, searchByCategory) => {
+    if (searchType === 'BY_TITLE') {
+      return filteredItemsByTitle(items, searchByTitle)
+    }
 
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow,
-            setProductToShow,
-            cartProducts,
-            setCartProducts,
-            isCheckoutSideMenuOpen,
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            order,
-            setOrder,
-            items,
-            setItems,
-            searchByTitle,
-            setSearchByTitle,
-            filteredItems,
-            searchByCategory,
-            setSearchByCategory,
-            account,
-            setAccount,
-            signOut,
-            setSignOut,
+    if (searchType === 'BY_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory)
+    }
 
-        }} >
-            {children}
-        </ShoppingCartContext.Provider>
-        
-    );
+    if (searchType === 'BY_TITLE_AND_CATEGORY') {
+      return filteredItemsByCategory(items, searchByCategory).filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    if (!searchType) {
+      return items
+    }
+  }
+
+  useEffect(() => {
+    if (searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_TITLE_AND_CATEGORY', items, searchByTitle, searchByCategory))
+    if (searchByTitle && !searchByCategory) setFilteredItems(filterBy('BY_TITLE', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && searchByCategory) setFilteredItems(filterBy('BY_CATEGORY', items, searchByTitle, searchByCategory))
+    if (!searchByTitle && !searchByCategory) setFilteredItems(filterBy(null, items, searchByTitle, searchByCategory))
+  }, [items, searchByTitle, searchByCategory])
+
+  return (
+    <ShoppingCartContext.Provider value={{
+      count,
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts,
+      isCheckoutSideMenuOpen,
+      openCheckoutSideMenu,
+      closeCheckoutSideMenu,
+      order,
+      setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle,
+      filteredItems,
+      searchByCategory,
+      setSearchByCategory,
+      account,
+      setAccount,
+      signOut,
+      setSignOut
+    }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
 }
